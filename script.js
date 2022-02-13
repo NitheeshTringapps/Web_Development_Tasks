66b6167ddf0070ea8ef9dde5bd8367feb07bc0e2
NitheeshTringapps <96728775+NitheeshTringapps@users.noreply.github.com>
2022-02-13 11:28:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ for(let i = 0; i < Tab.length; i++){
         this.classList.toggle('active');
     })
 }
+function newTab(event){
+    event.preventDefault();
+    var myContainer=document.getElementById("container");
+    var newCollapseTab=document.createElement("div");
+    newCollapseTab.setAttribute("class","collapseTab");
+
+    var heading=document.createElement("h1");
+    heading.setAttribute("class", "heading");
+    heading.innerText=document.getElementById("newHeading").value;
+    
+    var content=document.createElement("p");
+    content.setAttribute("class", "content");
+    content.innerText=document.getElementById("newContent").value;
+    
+    newCollapseTab.appendChild(heading);
+    newCollapseTab.appendChild(content);
+    myContainer.appendChild(newCollapseTab);
+    
+    newCollapseTab.addEventListener('click', function(){
+        this.classList.toggle('active');
+    })
+}
